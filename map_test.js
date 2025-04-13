1e85553cf2c1197c26c6b3dc33851eb17dea823c
nikolinnav <vasic_nikolina@yahoo.com>
2025-04-13 18:56:57 +0200

    add test functions for the map
    An array of objects of all the target places. Test functions: get the user's distance from the target place; save the visited place in localStorage; get the next location; display the tasks at a given location; set a user marker.

---
+++ map_test.js
@@ -0,0 +1,106 @@
+//Array of objects for all locations with ifo about their name, latitude and longitute.
+const locations = [
+    {
+        name: "Grannen",
+        lat: 55.60763,
+        lon: 12.98699
+    },
+]
+
+let currentIndex = 0;
+let notified = false;
+let targetMarker;
+let userMarker;
+
+const map = L.map('map').setView([0, 0], 17);
+L.tileLyer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//Calculates the user's distance from the target place.
+function getDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371e3;
+    const φ1 = lat1 * Math.PI / 180;
+    const φ2 = lat2 * Math.PI / 180;
+    const Δφ = (lat2 - lat1) * Math.PI / 180;
+    const Δλ = (lon2 - lon1) * Math.PI / 180;
+
+    const a = Math.sin(Δφ / 2) ** 2 +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ / 2) ** 2;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c;
+}
+
+function loadVisited() {
+    return JSON.parse(localStorage.getItem("visitedPlaces") || "[]");
+}
+
+function saveVisited(name) {
+    const visited = loadVisited();
+    if (!visited.includes(name)) {
+        visited.push(name);
+        localStorage.SetItem("visitedPlaces", JSON.stringify(visited));
+    }
+}
+
+function getNextLocation() {
+    const visited = loadVisited();
+    return locations.find(loc => !visited.includes(loc.name));
+}
+
+function setTarget(location) {
+    if (targetMarker) map.removeLayer(targetMarker);
+    if (!location) {
+        //UI display when all locations are visited. 
+        return;
+    }
+
+    targetMarker = L.marker([location.lat], [location.lon])
+        .addTo(map)
+        .bindPopup(location.name)
+        .openPopup();
+}
+
+const initialTarget = getNextLocation();
+setTarget(initialTarget);
+
+userMarker = L.marker([0, 0].addTo(map));
+
+if ("geolocation" in navigator) {
+    navigator.geolocation.watchPosition((pos) => {
+        const userLat = pos.coords.latitude;
+        const userLon = pos.coords.longitude;
+
+        userMarker.setLatLng([userLat, userLon]);
+        map.setView([userLat, userLon]);
+
+        const currentTarget = getNextLocation();
+        if (!currentTarget) return;
+
+        const dist = getDistance(userLat, userLon, currentTarget.lat, currentTarget.lon);
+        //UI display the distance to the current target in m. 
+        // Ex:
+        // document.getElementById("info").textContent =
+        // `Distance to ${currentTarget.name}: ${dist.toFixed(1)} m`;
+
+
+        if (dist < 2 && !notified) {
+            notified = true;
+            alert('Du har ankommit till ${currentTarget.name}');
+            saveVisited(currentTarget.name);
+
+            //Trigger the next task here. (interview, view a video, answer a question etc...)
+
+            //Show the next location
+            const nextTarget = getNextLocation();
+            setTarget(nextTarget);
+            notified = false;
+        }
+
+    }, err => {
+        console.error("Error", err);
+    }, {
+        enableHighAccuracy: true,
+        maximumAge: 1000,
+        timeout: 500
+    });
+}
