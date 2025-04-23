100d11a422bdda5d8189b3b1faaaa2394c5eab2c
Hampus833 <hampuse266@gmail.com>
2025-04-23 11:40:48 +0200

    starting to add audio

--- components/interviewPage/interviewPage.js
+++ components/interviewPage/interviewPage.js
@@ -8,42 +8,42 @@ function renderInterviewPage(questions) {
     <h2 class="interviewTitle">Intervjua Grannen</h2>
 
     <div class="interviewBoxesContainer">
-        <div class="interviewBoxes">
-            <p>${questions[0]}</p>
-            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="18" viewBox="0 0 17 18" fill="none">
-                <path d="M0 2.27387C0 0.567007 1.82609 -0.518303 3.32538 0.297487L15.687 7.02369C17.2531 7.87579 17.2531 10.1242 15.687 10.9764L3.32538 17.7026C1.82609 18.5183 0 17.433 0 15.7262V2.27387Z" fill="#212121"/>
+        <div class="question1 interviewBoxes">
+            <p class="question1">${questions[0]}</p>
+            <svg class="question1" xmlns="http://www.w3.org/2000/svg" width="17" height="18" viewBox="0 0 17 18" fill="none">
+                <path class="question1" d="M0 2.27387C0 0.567007 1.82609 -0.518303 3.32538 0.297487L15.687 7.02369C17.2531 7.87579 17.2531 10.1242 15.687 10.9764L3.32538 17.7026C1.82609 18.5183 0 17.433 0 15.7262V2.27387Z" fill="#212121"/>
             </svg>             
         </div>
         <div class="bar"></div>
 
-        <div class="interviewBoxes">
-            <p>${questions[1]}</p>
-            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="18" viewBox="0 0 17 18" fill="none">
-                <path d="M0 2.27387C0 0.567007 1.82609 -0.518303 3.32538 0.297487L15.687 7.02369C17.2531 7.87579 17.2531 10.1242 15.687 10.9764L3.32538 17.7026C1.82609 18.5183 0 17.433 0 15.7262V2.27387Z" fill="#212121"/>
+        <div class="question2 interviewBoxes">
+            <p class="question2">${questions[1]}</p>
+            <svg class="question2" xmlns="http://www.w3.org/2000/svg" width="17" height="18" viewBox="0 0 17 18" fill="none">
+                <path class="question2" d="M0 2.27387C0 0.567007 1.82609 -0.518303 3.32538 0.297487L15.687 7.02369C17.2531 7.87579 17.2531 10.1242 15.687 10.9764L3.32538 17.7026C1.82609 18.5183 0 17.433 0 15.7262V2.27387Z" fill="#212121"/>
             </svg>                     
         </div>
         <div class="bar"></div>
 
-        <div class="interviewBoxes">
-            <p>${questions[2]}</p>
-            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="18" viewBox="0 0 17 18" fill="none">
-                <path d="M0 2.27387C0 0.567007 1.82609 -0.518303 3.32538 0.297487L15.687 7.02369C17.2531 7.87579 17.2531 10.1242 15.687 10.9764L3.32538 17.7026C1.82609 18.5183 0 17.433 0 15.7262V2.27387Z" fill="#212121"/>
+        <div class="question3 interviewBoxes">
+            <p class="question3">${questions[2]}</p>
+            <svg class="question3" xmlns="http://www.w3.org/2000/svg" width="17" height="18" viewBox="0 0 17 18" fill="none">
+                <path class="question3" d="M0 2.27387C0 0.567007 1.82609 -0.518303 3.32538 0.297487L15.687 7.02369C17.2531 7.87579 17.2531 10.1242 15.687 10.9764L3.32538 17.7026C1.82609 18.5183 0 17.433 0 15.7262V2.27387Z" fill="#212121"/>
             </svg>             
         </div>
         <div class="bar"></div>
 
-        <div class="interviewBoxes">
-            <p>${questions[3]}</p>
-            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="18" viewBox="0 0 17 18" fill="none">
-                <path d="M0 2.27387C0 0.567007 1.82609 -0.518303 3.32538 0.297487L15.687 7.02369C17.2531 7.87579 17.2531 10.1242 15.687 10.9764L3.32538 17.7026C1.82609 18.5183 0 17.433 0 15.7262V2.27387Z" fill="#212121"/>
+        <div class="question4 interviewBoxes">
+            <p class="question4">${questions[3]}</p>
+            <svg class="question4" xmlns="http://www.w3.org/2000/svg" width="17" height="18" viewBox="0 0 17 18" fill="none">
+                <path class="question4" d="M0 2.27387C0 0.567007 1.82609 -0.518303 3.32538 0.297487L15.687 7.02369C17.2531 7.87579 17.2531 10.1242 15.687 10.9764L3.32538 17.7026C1.82609 18.5183 0 17.433 0 15.7262V2.27387Z" fill="#212121"/>
             </svg>              
         </div>
         <div class="bar"></div>
 
-        <div class="interviewBoxes">
-            <p>${questions[4]}</p>
-            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="18" viewBox="0 0 17 18" fill="none">
-                <path d="M0 2.27387C0 0.567007 1.82609 -0.518303 3.32538 0.297487L15.687 7.02369C17.2531 7.87579 17.2531 10.1242 15.687 10.9764L3.32538 17.7026C1.82609 18.5183 0 17.433 0 15.7262V2.27387Z" fill="#212121"/>
+        <div class="question5 interviewBoxes">
+            <p class="question5">${questions[4]}</p>
+            <svg class="question5" xmlns="http://www.w3.org/2000/svg" width="17" height="18" viewBox="0 0 17 18" fill="none">
+                <path class="question5" d="M0 2.27387C0 0.567007 1.82609 -0.518303 3.32538 0.297487L15.687 7.02369C17.2531 7.87579 17.2531 10.1242 15.687 10.9764L3.32538 17.7026C1.82609 18.5183 0 17.433 0 15.7262V2.27387Z" fill="#212121"/>
             </svg>          
         </div>
         <div class="bar"></div>
@@ -57,6 +57,35 @@ function renderInterviewPage(questions) {
     }
 }
 
-function playQuestion(event) {
-    console.log("now playing");
+async function playQuestion(event) {
+    // console.log("now playing");
+    // console.log(event.originalTarget);
+    let target = event.originalTarget.classList;
+
+    switch (target[0]) {
+        case "question1":
+            console.log("now playing question 1");
+            let url = "../../media/audio/Granne_fråga_1.mp3";
+            playAudio(url);
+            break;
+        case "question2":
+            console.log("now playing question 2");
+            break;
+        case "question3":
+            console.log("now playing question 3");
+            break;
+        case "question4":
+            console.log("now playing question 4");
+            break;
+        case "question5":
+            console.log("now playing question 5");
+            break;
+        default:
+            break;
+    }
+}
+
+function playAudio(url) {
+    let question = new Audio(url);
+    console.log(question);
 }
